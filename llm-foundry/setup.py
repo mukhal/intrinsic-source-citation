141ae91547d62692859f1e8455a7752f7f11d685
Muhammad Khalifa <muhammad.e.khalifa@gmail.com>
2025-04-01 12:30:44 -0400

    Update setup.py

--- llm-foundry/setup.py
+++ llm-foundry/setup.py
@@ -50,7 +50,7 @@ install_requires = [
     'composer[libcloud,nlp,wandb]>=0.15.0,<0.16',
     'accelerate>=0.20.3',  # for HF inference `device_map`
     'mosaicml-streaming>=0.5.1,<0.6',
-    'datasets==2.20.0',
+    'datasets==2.10.1',
     'sentencepiece==0.1.97',
     'einops==0.5.0',
     'omegaconf>=2.2.3,<3',
